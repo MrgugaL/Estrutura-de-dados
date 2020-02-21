85ba9722b777ae4ed530b1cb0a39a7b258f2a3e6
Gustavo Lucas da Silva <guga12384@gmail.com>
2020-02-21 20:13:40 -0300

    Add files via upload

---
+++ Faesa.dados/src/dados/Item.java
@@ -0,0 +1,21 @@
+package dados;
+
+public class Item {
+
+	private Item chave;
+
+	public Item(Item chave) {
+		this.chave = chave;
+
+	}
+	
+	public void setChave(Item chave) {
+		this.chave = chave;
+	}
+
+	public Item getChave() {
+		return chave;
+	}
+
+
+}
---
+++ Faesa.dados/src/simplesmente/ListaSimples.java
@@ -0,0 +1,15 @@
+package simplesmente;
+
+public class ListaSimples {
+	private No prim;
+	private No ult;
+	private int quantNos;
+
+	public ListaSimples() {
+		this.prim = null;
+		this.ult = null;
+		this.quantNos = 0;
+
+	}
+
+}
